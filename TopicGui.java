1f13c6797b130dd577e5b4d034fb10bf58be52ff
ethanm32 <c19305451@mytudublin.ie>
2021-03-23 23:21:00 +0000

    Second file chooser
    Adding another button that takes in a second file to an array list

--- TopicGui.java
+++ TopicGui.java
@@ -1,12 +1,17 @@
 package com.topic.modeller;
 
+import java.awt.FlowLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JPanel;
+
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 import javax.swing.JFileChooser;
@@ -14,35 +19,45 @@ import javax.swing.JFileChooser;
 public class TopicGui extends JFrame implements ActionListener
 
 {
-	JButton Button1;
+	JButton Button1,Button2;
+	JPanel panel;
 	final JFileChooser fc = new JFileChooser();
 	TopicGui(String title)
 	{
 		super(title);
 		setSize(300,300);
+		setLayout(new FlowLayout());
 		Button1 = new JButton("Select a file");
+		Button2 = new JButton("Select a file to compare");
 		Button1.addActionListener(this);
-		add(Button1);
+		Button2.addActionListener(this);
+		panel = new JPanel(new FlowLayout());
+		panel.add(Button1);
+		panel.add(Button2);
+		add(panel);
 		setVisible(true);
 	}
-	
 	
 
 	@Override
 	public void actionPerformed(ActionEvent arg0) {
 		// TODO Auto-generated method stub
+		List<String> file_1 = new ArrayList<String>();
+		List<String> file_2 = new ArrayList<String>();
 		if(arg0.getSource() == Button1)
 		{
-			int op = fc.showOpenDialog(Button1);
-			if (op == JFileChooser.APPROVE_OPTION)
+			int buttonChoose = fc.showOpenDialog(Button1);
+			if (buttonChoose == JFileChooser.APPROVE_OPTION)
 			{
 				File file1 = fc.getSelectedFile();
 				try {
 					Scanner newScan = new Scanner(file1);
 					while (newScan.hasNextLine())
 					{
-						String newLine = newScan.nextLine();
-						System.out.println(newLine);
+						file_1.add(newScan.nextLine());
+						for(int i = 0;i < file_1.size(); i++) {
+							System.out.println(file_1.get(i));
+						}
 					}
 					newScan.close();
 				} catch (FileNotFoundException e) {
@@ -52,5 +67,29 @@ public class TopicGui extends JFrame implements ActionListener
 				
 			}
 		}
+		else if(arg0.getSource() == Button2)
+		{
+			int buttonChoose2 = fc.showOpenDialog(Button1);
+			if (buttonChoose2 == JFileChooser.APPROVE_OPTION)
+			{
+				File file2 = fc.getSelectedFile();
+				try {
+					Scanner newScan2 = new Scanner(file2);
+					while (newScan2.hasNextLine())
+					{
+						file_2.add(newScan2.nextLine());
+						for(int i = 0;i < file_2.size(); i++) {
+							System.out.println(file_2.get(i));
+						}
+					}
+					newScan2.close();
+				} catch (FileNotFoundException e) {
+					// TODO Auto-generated catch block
+					System.out.println("Not found");
+				}
+				
+			}
 	}
+ }
+	
 }
